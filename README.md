c82c73177ae01be96b9fbe9886d8de3cdf184af7
edanTal <edant81@gmail.com>
2024-08-30 01:26:52 +0200

    Setup search UI

---
+++ README.md
@@ -0,0 +1,48 @@
+# Weather React/Typescript/TaliwindCSS App
+
+<div>
+  <div>
+    <img src="https://img.shields.io/badge/-React_JS-black?style=for-the-badge&logoColor=149eca&logo=react&color=16181d" alt="react.js" />
+    <img src="https://img.shields.io/badge/-Typescript-black?style=for-the-badge&logoColor=ffffff&logo=typescript&color=007acc" alt="typescript" />
+    <img src="https://img.shields.io/badge/-Tailwind_CSS-black?style=for-the-badge&logoColor=white&logo=tailwindcss&color=06b6d4" alt="tailwindcss" />
+  </div>
+  <h3>ReactJS/Typescript/TailwindCSS project with Weather API</h3>
+</div>
+
+This repository is using ReactJS, TypeScript and TailwindCSS. It runs on node 18 (see `.nvmrc` file).
+
+---
+
+### How To Run
+
+- If you don't have nvm installed, run the following command:
+
+  ```
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  ```
+
+- After you got nvm installed, run `nvm i` and then `nvm use`
+
+- Run `npm i` to get all the required dependencies installed
+
+- Run `npm start` and navigate to [http://localhost:3000](http://localhost:3000)
+
+### API
+
+### Resources
+
+- [Tailwind CSS](https://tailwindcss.com)
+
+<!-- ### Preview
+
+[View Design]() -->
+
+<!--
+
+---
+
+### Source
+
+[TYPESCRIPT REACT API PROJECT USING A WEATHER API](https://youtu.be/6MKFKwwhbNo?si=Df06gSU5ISlXLMfI)
+
+-->
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import SearchLineIcon from 'remixicon-react/SearchLineIcon'
+
 const App = () => {
   return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
-      Hello world
+    <main className='flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full'>
+      <section className='w-full md:max-w-[700px] h-full lg:h-[600px] flex flex-col items-center justify-center p-4 md:px-10 lg:p-24 bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded text-center text-zinc-700'>
+        <h1 className='text-4xl font-extralight'>
+          Die <span className='font-extrabold'>Wettervorhersage</span>
+        </h1>
+        <p className='text-sm mt-2'>
+          Geben Sie einen Ort ein, für den Sie eine Wettervorhersage erhalten
+          möchten, und wählen Sie eine Option aus der Dropdown-Liste
+        </p>
+        <div className='flex mt-10 md:mt-4'>
+          <input
+            type='text'
+            value={''}
+            className='px-2 py-1 rounded-l-md border-2 border-white placeholder:font-light placeholder:text-sm placeholder:italic'
+            placeholder='Ort...'
+          />
+          <button className='flex px-2 py-1 rounded-r-md border-2 border-zinc-100 hover:border-zinc-700 bg-transparent hover:bg-zinc-700 text-white hover:text-white cursor-pointer transition-all'>
+            <SearchLineIcon className='mr-1' /> Suchen
+          </button>
+        </div>
+      </section>
     </main>
   )
 }

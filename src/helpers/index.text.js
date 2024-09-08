5f6fc03907b442caa180ce2f779451b5cadf1dbe
edanTal <edant81@gmail.com>
2024-09-08 11:54:16 +0200

    Create seperate helpers text file

---
+++ src/helpers/index.text.js
@@ -0,0 +1,29 @@
+export const TEXTS = {
+  windDirection: {
+    N: 'N',
+    NE: 'NE',
+    E: 'E',
+    SE: 'SE',
+    S: 'S',
+    SW: 'SW',
+    W: 'W',
+    NW: 'NW',
+  },
+  humidityValue: {
+    low: 'Dry and comfortable',
+    moderate: 'A bit uncomfortable, sticky feeling',
+    high: 'Lots of moisture, uncomfortable air',
+  },
+  visibilityValue: {
+    extremeLow: 'Dangerously foggy',
+    veryLow: 'Expect heavy fog',
+    low: 'Expect some fog',
+    moderate: 'Expect some haze',
+    high: 'Very clear day',
+  },
+  popValue: {
+    low: 'Low probability',
+    moderate: 'Moderate probability',
+    high: 'High probability',
+  },
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,32 +1,34 @@
+import { TEXTS } from './index.text'
+
 export const getWindDirection = (deg: number): string => {
-  if (deg > 15 && deg <= 75) return 'NE'
+  if (deg > 15 && deg <= 75) return TEXTS.windDirection.NE
 
-  if (deg > 76 && deg <= 105) return 'E'
-  if (deg > 105 && deg <= 165) return 'SE'
+  if (deg > 76 && deg <= 105) return TEXTS.windDirection.E
+  if (deg > 105 && deg <= 165) return TEXTS.windDirection.SE
 
-  if (deg > 166 && deg <= 195) return 'S'
-  if (deg > 195 && deg <= 255) return 'SW'
+  if (deg > 166 && deg <= 195) return TEXTS.windDirection.S
+  if (deg > 195 && deg <= 255) return TEXTS.windDirection.SW
 
-  if (deg > 255 && deg <= 285) return 'W'
-  if (deg > 285 && deg <= 345) return 'NW'
+  if (deg > 255 && deg <= 285) return TEXTS.windDirection.W
+  if (deg > 285 && deg <= 345) return TEXTS.windDirection.NW
 
-  return 'N'
+  return TEXTS.windDirection.N
 }
 
 export const getHumidityValue = (level: number): string => {
-  if (level <= 55) return 'Dry and comfortable'
-  if (level > 55 && level <= 65) return 'A bit uncomfortable, sticky feeling'
+  if (level <= 55) return TEXTS.humidityValue.low
+  if (level > 55 && level <= 65) return TEXTS.humidityValue.moderate
 
-  return 'Lots of moisture, uncomfortable air'
+  return TEXTS.humidityValue.high
 }
 
 export const getVisibilityValue = (number: number): string => {
-  if (number <= 50) return 'Dangerously foggy'
-  if (number > 50 && number <= 500) return 'Expect heavy fog'
-  if (number > 500 && number <= 2000) return 'Expect some fog'
-  if (number > 2000 && number <= 9000) return 'Expect some haze'
+  if (number <= 50) return TEXTS.visibilityValue.extremeLow
+  if (number > 50 && number <= 500) return TEXTS.visibilityValue.veryLow
+  if (number > 500 && number <= 2000) return TEXTS.visibilityValue.low
+  if (number > 2000 && number <= 9000) return TEXTS.visibilityValue.moderate
 
-  return 'Very clear day'
+  return TEXTS.visibilityValue.high
 }
 
 export const getSunTime = (timestamp: number): string => {
@@ -41,8 +43,8 @@ export const getSunTime = (timestamp: number): string => {
 }
 
 export const getPop = (value: number): string => {
-  if (value <= 0.33) return 'Low probability'
-  if (value > 0.33 && value <= 0.66) return 'Moderate probability'
+  if (value <= 0.33) return TEXTS.popValue.low
+  if (value > 0.33 && value <= 0.66) return TEXTS.popValue.moderate
 
-  return 'High probability'
+  return TEXTS.popValue.high
 }
